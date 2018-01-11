6210b13f1beb28af8b4c80f0895ff1a4e1200468
Herve Nicol <herve.nicol@gmail.com>
2018-01-11 23:22:41 +0100

    readme

--- README.tf
+++ README.tf
@@ -6,13 +6,19 @@
 * http://www.terraform.io/downloads.html
 * https://www.digitalocean.com/community/tutorials/how-to-use-terraform-with-digitalocean
 
-=== kubeadm ===
-installed per node
+=== ansible ===
+
+== What it does ==
+creates nodes on DO
+
+Install kubeadm on them
 * https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
 * https://kubernetes.io/docs/setup/independent/install-kubeadm/
 
+and deploy kubernetes through kubeadm
 
 == Run ==
+* ./gen_nodes_from_manager1.sh
 * eval terraform plan $(./params.sh)
 * eval terraform apply $(./params.sh)
 * eval terraform destroy $(./params.sh)
